c06ef4a5ccea52d0109110339fb9536823b8564b
Werner Oswald <osiworx@gmail.com>
2023-05-20 13:14:44 +0200

    .

--- conditioning_nodes/many_prompts_node.py
+++ conditioning_nodes/many_prompts_node.py
@@ -33,11 +33,12 @@ class ManyPromptsNode(AiNode):
 	op_title = "Many Prompts Node"
 	content_label_objname = "many_prompts_node"
 	category = "Data"
-	# custom_input_socket_name = ["DOG", "CAT", "42"]
+	custom_input_socket_name = ['DONE', "DATA", "EXEC"]
+
 	custom_output_socket_name = ['DONE', "DATA", "EXEC"]
 
 	def __init__(self, scene):
-		super().__init__(scene, inputs=[6, 1], outputs=[1, 3, 1])
+		super().__init__(scene, inputs=[1, 6, 1], outputs=[1, 6, 1])
 
 	def initInnerClasses(self):
 		self.content = ManyPromptsWidget(self)
