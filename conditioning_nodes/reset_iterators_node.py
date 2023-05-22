72b00c172b79b07c5192cc4c5e5cccc09b0d070b
Werner Oswald <osiworx@gmail.com>
2023-05-22 06:20:48 +0200

    .

--- conditioning_nodes/reset_iterators_node.py
+++ conditioning_nodes/reset_iterators_node.py
@@ -49,7 +49,7 @@ class ManyPromptsNode(AiNode):
 		self.content.setMinimumWidth(340)
 		self.content.setMinimumHeight(500)
 		self.content.eval_signal.connect(self.evalImplementation)
-		self.reset_signal = dispatcher.Signal()
+
 
 
 	@QtCore.Slot()
